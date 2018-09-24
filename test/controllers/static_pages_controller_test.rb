410d070490a5f5f956a05145498bd4d8242afb52
u-kou <u-kou@ist.osaka-u.ac.jp>
2018-09-24 23:23:39 +0900

    Finish static pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,13 +1,14 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
-  test "should get home" do
-    get static_pages_home_url
+  test "should get contact" do
+    get static_pages_contact_url
     assert_response :success
+    assert_select "title", "Contact | Ruby on Rails Tutorial Sample App"
   end
 
-  test "should get help" do
-    get static_pages_help_url
+  test "should get root" do
+    get root_url
     assert_response :success
   end
 
