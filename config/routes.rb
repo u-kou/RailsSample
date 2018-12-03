6bd46063a487d6a8a40591429264a231a9b6001a
u-kou <u-kou@ist.osaka-u.ac.jp>
2018-12-03 15:18:00 +0900

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
   get '/contact',	to: 'static_pages#contact'
   get '/about',	to: 'static_pages#about'
   get '/signup',	to: 'users#new'
+  post '/signup',	to: 'users#create'
   resources :books
+  resources :users
   root 'static_pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
